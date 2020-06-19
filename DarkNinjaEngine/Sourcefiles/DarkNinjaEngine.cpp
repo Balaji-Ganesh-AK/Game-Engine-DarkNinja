e81277ae664eb09cc44ba76de3b8f44b46d00908
Balaji <balajiganeshoff@gmail.com>
2020-06-18 19:29:33 -0600

    Starting New Project.

---
+++ DarkNinjaEngine/Sourcefiles/DarkNinjaEngine.cpp
@@ -0,0 +1,15 @@
+// DarkNinjaEngine.cpp : Defines the functions for the static library.
+//
+
+#include <iostream>
+#include "Engine.h"
+
+namespace Engine
+{
+	
+// TODO: This is an example of a library function
+void Start()
+{
+	std::cout << "Gaming Engine Started!"<<std::endl;
+}
+}
---
+++ PongGame/PongGame.cpp
@@ -0,0 +1,11 @@
+
+
+#include <iostream>
+#include "../DarkNinjaEngine/Sourcefiles/Engine.h"
+
+int main()
+{
+	Engine::Start();
+    std::cout << "Hello World!\n";
+}
+
